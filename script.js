02078607bfdbffaa5bb09ae5c0d4361777d8bd8e
Brendo <brendowashingtone@gmail.com>
2024-07-15 08:30:44 -0300

    Ficou bom

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-
 const botao = document.getElementById('botao');
-const msg = document.getElementById('msg');
 const resultado = document.createElement('span');
-const container = document.getElementById('main');
 const img = document.getElementById('img');
-
-
 const sd = document.getElementById('sd');
 
 botao.addEventListener('click', function () {
     const email = document.getElementById('email').value;
     
-    if (email.indexOf('@gmail.com') == email.length - 10) {
-        resultado.classList.add('confirmar')
+    if (email.includes('@gmail.com') && email.indexOf('@gmail.com') == email.length - 10) {
+        resultado.style.color = 'blue';
         resultado.innerHTML = '<p>Nice</p>'
         sd.appendChild(resultado)
 
     } else {
-        resultado.classList.add('erro')
+        resultado.style.color = 'red';
         resultado.innerHTML = '<p>Please provide a valid email address</p>'
         sd.appendChild(resultado)
         document.getElementById('email').style.borderColor = 'red';
